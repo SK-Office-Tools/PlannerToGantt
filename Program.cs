9026eedcd03701b18563ecda7717cb6864822883
Steve Kay <stephen.kay@methods.co.uk>
2022-02-22 11:35:56 +0000

    working
    Added sample data and got it rendering

--- Program.cs
+++ Program.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using PlannerToGantt;
+using Syncfusion.Blazor;
 
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NTg1NTQ3QDMxMzkyZTM0MmUzMFU4Y3U4bGlvMG5UV3MxUzZOVmplRWVwazZDWWt2TG1sZy9OMGFQcjdLdlk9");
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSyncfusionBlazor(options => { options.IgnoreScriptIsolation = true; });
 
 await builder.Build().RunAsync();
 
