d6434bb36cfa485d090fe1e65e82cc5529b9c8e3
Steve Kay <stephen.kay@methods.co.uk>
2022-03-10 20:42:58 +0000

    Update PlannerDataMap.cs
    Should deal with locale issues concerning the date

--- Data/PlannerDataMap.cs
+++ Data/PlannerDataMap.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using CsvHelper;
 using CsvHelper.Configuration;
 using CsvHelper.TypeConversion;
@@ -27,16 +28,17 @@ namespace PlannerToGantt.Data
 	{
 		public PlannerDataMap()
 		{
-			Map(m => m.StartDate).Name("Start Date").TypeConverterOption.Format("dd/MM/yyyy");  //	Created Date
-			Map(m => m.EndDate).Name("Due Date").TypeConverterOption.Format("dd/MM/yyyy");  // Completed Date
+			var shortDateFormat = CultureInfo.CurrentCulture.DateTimeFormat.ShortDatePattern;
+			Map(m => m.StartDate).Name("Start Date").TypeConverterOption.Format(shortDateFormat);  //	Created Date
+			Map(m => m.EndDate).Name("Due Date").TypeConverterOption.Format(shortDateFormat);  // Completed Date
 			Map(m => m.Id).Name("Task ID");
 			Map(m => m.TaskName).Name("Task Name");
 			Map(m => m.AssignedTo).Name("Assigned To");
 			Map(m => m.Priority).Name("Priority");
 			Map(m => m.Status).Name("Progress");
 			Map(m => m.BucketName).Name("Bucket Name");
-			Map(m => m.CreatedDate).Name("Created Date").TypeConverterOption.Format("dd/MM/yyyy");
-			Map(m => m.CompletedDate).Name("Completed Date").TypeConverterOption.Format("dd/MM/yyyy");
+			Map(m => m.CreatedDate).Name("Created Date").TypeConverterOption.Format(shortDateFormat);
+			Map(m => m.CompletedDate).Name("Completed Date").TypeConverterOption.Format(shortDateFormat);
 			Map(m => m.Labels).Name("Labels").TypeConverter<ListConverter>();
 			Map(m => m.ChecklistItems).Name("Checklist Items").TypeConverter<ListConverter>();
 		}
