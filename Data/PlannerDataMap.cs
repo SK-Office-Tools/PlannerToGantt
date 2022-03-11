6eb87a7a68ce067071e5f20c150d996b875abcd6
Steve Kay <stephen.kay@methods.co.uk>
2022-03-11 19:31:29 +0000

    updates

--- Data/PlannerDataMap.cs
+++ Data/PlannerDataMap.cs
@@ -26,22 +26,30 @@ namespace PlannerToGantt.Data
 
 	public class PlannerDataMap : ClassMap<TaskData>
 	{
+		public string ShortDateFormat { get; set; } = CultureInfo.CurrentCulture.DateTimeFormat.ShortDatePattern;
 		public PlannerDataMap()
 		{
-			var shortDateFormat = CultureInfo.CurrentCulture.DateTimeFormat.ShortDatePattern;
-			Map(m => m.StartDate).Name("Start Date").TypeConverterOption.Format(shortDateFormat);  //	Created Date
-			Map(m => m.EndDate).Name("Due Date").TypeConverterOption.Format(shortDateFormat);  // Completed Date
+			if (string.IsNullOrEmpty(ShortDateFormat))
+				ShortDateFormat = CultureInfo.CurrentCulture.DateTimeFormat.ShortDatePattern;
+
+			Map(m => m.StartDate).Name("Start Date").TypeConverterOption.Format(ShortDateFormat);  //	Created Date
+			Map(m => m.EndDate).Name("Due Date").TypeConverterOption.Format(ShortDateFormat);  // Completed Date
 			Map(m => m.Id).Name("Task ID");
 			Map(m => m.TaskName).Name("Task Name");
 			Map(m => m.AssignedTo).Name("Assigned To");
 			Map(m => m.Priority).Name("Priority");
 			Map(m => m.Status).Name("Progress");
 			Map(m => m.BucketName).Name("Bucket Name");
-			Map(m => m.CreatedDate).Name("Created Date").TypeConverterOption.Format(shortDateFormat);
-			Map(m => m.CompletedDate).Name("Completed Date").TypeConverterOption.Format(shortDateFormat);
+			Map(m => m.CreatedDate).Name("Created Date").TypeConverterOption.Format(ShortDateFormat);
+			Map(m => m.CompletedDate).Name("Completed Date").TypeConverterOption.Format(ShortDateFormat);
 			Map(m => m.Labels).Name("Labels").TypeConverter<ListConverter>();
 			Map(m => m.ChecklistItems).Name("Checklist Items").TypeConverter<ListConverter>();
 		}
 	}
+
+	public class PlannerDataMapUK : PlannerDataMap
+	{
+		public PlannerDataMapUK() : base() { ShortDateFormat = "dd/MM/yyyy"; }
+			}
 }
 
