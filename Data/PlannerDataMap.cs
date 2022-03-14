8d59754694fd68f0041f1f23c1207070ad69710b
Steve Kay <stephen.kay@methods.co.uk>
2022-03-14 16:18:04 +0000

    Fixed
    Not a great fix but at least it should load the files now

--- Data/PlannerDataMap.cs
+++ Data/PlannerDataMap.cs
@@ -26,22 +26,23 @@ namespace PlannerToGantt.Data
 
 	public class PlannerDataMap : ClassMap<TaskData>
 	{
-		public string ShortDateFormat { get; set; } = CultureInfo.CurrentCulture.DateTimeFormat.ShortDatePattern;
 		public PlannerDataMap()
+        {
+			var ShortDateFormat = CultureInfo.CurrentCulture.DateTimeFormat.ShortDatePattern;
+            _ = new PlannerDataMap(ShortDateFormat);
+		}
+		public PlannerDataMap(string dateFormat)
 		{
-			if (string.IsNullOrEmpty(ShortDateFormat))
-				ShortDateFormat = CultureInfo.CurrentCulture.DateTimeFormat.ShortDatePattern;
-
-			Map(m => m.StartDate).Name("Start Date").TypeConverterOption.Format(ShortDateFormat);  //	Created Date
-			Map(m => m.EndDate).Name("Due Date").TypeConverterOption.Format(ShortDateFormat);  // Completed Date
+			Map(m => m.StartDate).Name("Start Date").TypeConverterOption.Format(dateFormat);  //	Created Date
+			Map(m => m.EndDate).Name("Due Date").TypeConverterOption.Format(dateFormat);  // Completed Date
 			Map(m => m.Id).Name("Task ID");
 			Map(m => m.TaskName).Name("Task Name");
 			Map(m => m.AssignedTo).Name("Assigned To");
 			Map(m => m.Priority).Name("Priority");
 			Map(m => m.Status).Name("Progress");
 			Map(m => m.BucketName).Name("Bucket Name");
-			Map(m => m.CreatedDate).Name("Created Date").TypeConverterOption.Format(ShortDateFormat);
-			Map(m => m.CompletedDate).Name("Completed Date").TypeConverterOption.Format(ShortDateFormat);
+			Map(m => m.CreatedDate).Name("Created Date").TypeConverterOption.Format(dateFormat);
+			Map(m => m.CompletedDate).Name("Completed Date").TypeConverterOption.Format(dateFormat);
 			Map(m => m.Labels).Name("Labels").TypeConverter<ListConverter>();
 			Map(m => m.ChecklistItems).Name("Checklist Items").TypeConverter<ListConverter>();
 		}
@@ -49,7 +50,11 @@ namespace PlannerToGantt.Data
 
 	public class PlannerDataMapUK : PlannerDataMap
 	{
-		public PlannerDataMapUK() : base() { ShortDateFormat = "dd/MM/yyyy"; }
+		public PlannerDataMapUK() : base("dd/MM/yyyy") {}
 			}
+	public class PlannerDataMapUS : PlannerDataMap
+	{
+		public PlannerDataMapUS() : base("MM/dd/yyyy") {}
+	}
 }
 
